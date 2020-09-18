1e1a0ef4ca738a4b830b1e8d6a06f74a59618978
KB7860 <kyle.brown7860@gmail.com>
2020-09-18 13:46:44 +0000

    In progress, about to try to link with iap

--- main.js
+++ main.js
@@ -152,14 +152,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-     this.connector.get((data, error) => {
-    if (error) {
-      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-      return error
-    }
-    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-    return data
-  });
+     this.connector.get(callback);
   }
 
   /**
@@ -178,12 +171,7 @@ class ServiceNowAdapter extends EventEmitter {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-     this.connector.post((data, error) => {
-    if (error) {
-      console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
-    }
-    console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`)
-  });
+     this.connector.post(callback);
   }
 }
 
